e859f0e2d235d83c437cd1c445ca9045317b5512
Rodrigo Barroso <digorbarroso@gmail.com>
2022-04-27 17:16:04 -0300

    Updated 27/04

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from datetime import datetime
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 import time
-time.clock = time.time
 
+time.clock = time.time
 
 chatbot = ChatBot("BotRodrigo")
 conversa = [
-    "/start",
     "Como você está?",
     "Bem",
     "Que ótimo",
@@ -32,18 +31,23 @@ conversa = [
     "pois é né",
     "tamo junto",
     "tamo junto patrão",
+    "Qual é o seu nome?",
+    "Meu nome é bot do Rodrigo!"
 ]
 
 API_KEY = "5367731532:AAH5z61S8BRVrqltgUgrHR1YD6PV_Fy2TqQ"
 
 bot = telebot.TeleBot(API_KEY)
 
+#trainer = ListTrainer(chatbot)
+#trainer.train(conversa)
+
 
 def verificar(msg):
     print(msg.from_user.first_name)
     match msg.text:
         case "/start":
-            answer(msg)
+            responder(msg)
         case "/opcao1":
             show_temp_rio(msg)
         case "/opcao2":
@@ -56,11 +60,12 @@ def verificar(msg):
 
 @bot.message_handler(func=verificar)
 def responder(msg):
-    text = """Oi, eu sou o bot de Teste do Rodrigo, fui criado apenas para testes. 
-    Escolha uma opção para eu te ajudar :) (Clique no item):
-    /opcao1 Dizer a temperatura atual no Rio de Janeiro
-    /opcao2 Dizer a temperatura atual em Niterói
-    /opcao3 Mostrar o horário oficial de Brasília"""
+    text = f"""Oi {msg.from_user.first_name}, eu sou o bot de Teste do Rodrigo, fui criado apenas para testes. 
+                Escolha uma opção para eu te ajudar :) (Clique no item):
+                /opcao1 Dizer a temperatura atual no Rio de Janeiro
+                /opcao2 Dizer a temperatura atual em Niterói
+                /opcao3 Mostrar o horário oficial de Brasília
+                ou apenas converse comigo ;)"""
     bot.reply_to(msg, text)
 
 
@@ -78,6 +83,7 @@ def show_brasilia_time(msg):
     cur_time = datetime.now().strftime("%H:%M")
     text = f"O horário atual de Brasília é {cur_time}"
     bot.reply_to(msg, text)
+
 
 def answer(msg):
     bot.reply_to(msg, chatbot.get_response(msg.text))
